761cc14e3f6d3426bbb130ca267de49b77a4e8ad
vladimirskaya <63165081+vladimirskaya@users.noreply.github.com>
2021-03-19 01:16:01 +0300

    feat: ready

--- main_complex.js
+++ main_complex.js
@@ -1,100 +1,55 @@
-let getTime = function() {
+'use strict'
 
-	let today = new Date();
-		arrTime = [today.getDay(), today.getDate(), today.getMonth(), today.getFullYear(), today.getHours(), today.getMinutes(), today.getSeconds()];
-	
-	let correctWeekDay = function(wDay){
-			weekDays = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'];
-			for (let i = 0; i < weekDays.length;  i++) {
-				if (wDAy = i){
-					return weekDays[i];
-				}
-			} 
-		},
-	
-		correctMonth = function(month){
-			
-			monthNames = ['января', 'февраля','марта', 'апреля','мая', 'июня','июля', 'августа',
-						'сентября', 'октября','ноября', 'декабря']
-			for (let i = 0; i < monthNames.length;  i++) {
-				// console.log(i);
-				if (month = i){
-					return monthNames[i+1];
-				} 
-			}
-		},
-		
-		correctHours = function(hour){
-			//console.log(hour);
-			if (hour === 1 || hour === 21) {
-				return 'час';
-			} else if ((0 < hour && hour < 5) || (21 < hour && hour < 24)){
-				return 'часа';
-			} else {
-				return 'часов';
-			}
-		},
-		
-		correctMinutes = function(m){
-			if (m % 10 === 2 || m % 10 === 3 || m % 10 === 4){
-				return 'минуты';
-			} else if (m % 10 === 1 && m !== 11){
-				return 'минута';
-			} else {
-				return "минут"
-			}
-		},
-		
-		correctSeconds = function(sec){
-			if (sec % 10 === 2 || sec % 10 === 3 || sec % 10 === 4){
-				return 'секунды';
-			} else if (sec % 10 === 1 && sec !== 11){
-				return 'секунда';
-			} else {
-				return "секунд"
-			}
-		},
-		
-		editValue = function(n){
-			if ( +n < 10){
-				return '0' + n;
-			} else {
-				return n;
-			}
-		}
-		
-	return timeOptions = [`'Сегодня ${correctWeekDay(arrTime[0])}, ${arrTime[1]} ${correctMonth(arrTime[2])} ${arrTime[3]} года, ${arrTime[4]} ${correctHours(arrTime[4])} ${arrTime[5]} ${correctMinutes(arrTime[5])} ${arrTime[6]} ${correctSeconds(arrTime[6])}'`,
-				`'${editValue(arrTime[1])}.${editValue(arrTime[2] + 1)}.${editValue(arrTime[3])} - ${editValue(arrTime[4])}:${editValue(arrTime[5])}:${editValue(arrTime[6])}'`];
-		
-};
+const body = document.querySelector('body'),
+    color = document.getElementById('color'),
+    btnChange = document.getElementById('change');
+let num16 = localStorage.getItem('color');
 
-	// adding new divs
-let addElements = function(arr){
-	let container = document.getElementsByClassName('container')[0]
-	// console.log(arr);
-	for (let value of arr){
-		
-		div = document.createElement('div');
-		div.innerHTML = value;
-		div.className = 'what_time';
-		div.style.color = 'red';
-		div.style.fontWeight = 'bold';
-		
-		container.append(div); 
-		
-	}
-};
-	// delete the divs
-let delElements = function(){
-	let divs = document.querySelectorAll('.what_time')
-	
-	divs[0].remove();
-	divs[1].remove();
-	
-}
-addElements(getTime());
-let renewDate = setInterval(()=>{
-		delElements();
-		addElements(getTime());		
-	}, 1000);
-				
+const render = () => {
+    color.textContent = num16;
+    body.style.backgroundColor = num16;
+} 
+render();
+
+btnChange.addEventListener('click', () =>{
+    num16 = '#';
+    const changeInto16 = (n) => {
+        if (n > 9) {
+            switch (n) {
+                case 10:
+                    n = 'a';
+                break;
+                case 11:
+                    n = 'b';
+                break;
+                case 12:
+                    n = 'c';
+                break;
+                case 13:
+                    n = 'd';
+                break;
+                case 14:
+                    n = 'e';
+                break;
+                case 15:
+                    n = 'f';
+                break;
+            }
+        }
+        return String(n);
+    }
+
+    for (let i =0; i < 3; i++){
+        let num10 = Math.floor(Math.random() * 256),
+            firstPart16 = Math.trunc(num10 / 16),
+            secondPart16 = num10 % 16,
+            allParts16 = changeInto16(firstPart16) + changeInto16(secondPart16);
+            num16 += allParts16;
+    }
+    render();
+    
+})
+    
+window.addEventListener('unload', () =>{
+    localStorage.setItem('color', num16);
+})
